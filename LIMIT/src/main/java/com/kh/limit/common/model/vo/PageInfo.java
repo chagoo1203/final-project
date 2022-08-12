9050a346665af792ddfda8a4f0a4279a0c44d4b8
WonseokHahn <110151152+WonseokHahn@users.noreply.github.com>
2022-08-12 09:47:21 +0900

    매퍼파일 변경

--- LIMIT/src/main/java/com/kh/limit/common/model/vo/PageInfo.java
+++ LIMIT/src/main/java/com/kh/limit/common/model/vo/PageInfo.java
@@ -13,10 +13,10 @@ import lombok.ToString;
 @ToString
 public class PageInfo {
 
-	private int listCount;
-	private int currentPage;
-	private int pageLimit;
-	private int boardLimit;
+	private int listCount;//전체 리스트의 갯수
+	private int currentPage; //현재 페이지
+	private int pageLimit; //페이징 바의 한줄에 있는 최대 갯수
+	private int boardLimit; //한페이지에 리스트가 몇개까지 들어갈수잇는지
 	
 	private int maxPage;
 	private int startPage;
